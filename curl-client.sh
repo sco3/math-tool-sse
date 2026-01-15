c3712b97107feaf93852629c2c1900089bf7cff2
Dima Zakharov <zakharov@ibm.com>
2026-01-15 12:57:20 +0000

    works in non dev mode

--- curl-client.sh
+++ curl-client.sh
@@ -9,6 +9,27 @@ sleep 1
 endpoint=$(grep "data:" sse_output.txt | awk '{ print $2 }')
 # echo "Endpoint: $endpoint"
 
+
+curl -X POST -H "Content-Type: application/json" --data '{
+  "jsonrpc": "2.0",
+  "id": "1",
+  "method": "initialize",
+  "params": {
+    "protocolVersion": "2024-11-05",
+    "capabilities": {},
+    "clientInfo": {
+      "name": "my-curl-client",
+      "version": "1.0.0"
+    }
+  }
+}' "http://localhost:8080$endpoint"
+
+curl -X POST -H "Content-Type: application/json" --data '{
+  "jsonrpc": "2.0",
+  "method": "notifications/initialized"
+}' "http://localhost:8080$endpoint"
+
+
 curl --data '{
   "jsonrpc": "2.0",
   "id": 1,
